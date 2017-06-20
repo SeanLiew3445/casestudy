18cd7f1fb3ac018ff5b94c50ee0cab8568333881
SeanLiew <1502809e@student.tp.edu.sg>
2017-06-20 21:50:05 +0800

    STI_P02_T2_OpenVSwitch_Sean

--- backup.py
+++ backup.py
@@ -1,8 +1,8 @@
-import subprocess_sean, json
+import sub_sean, json
 from flask import Flask, jsonify, request, abort, make_response
 from flask.ext.httpauth import HTTPBasicAuth
 
-cs = Flask(__name__)
+call = Flask(__name__)
 auth = HTTPBasicAuth()
 
 
@@ -18,32 +18,32 @@ def get_password(username):
 def unauthorised():
 	return make_response(jsonify( { 'error': 'Unauthorised access' } ), 403)
 
-@cs.errorhandler(400)
+@call.errorhandler(400)
 def not_found(error):
 	return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
-@cs.errorhandler(404)
+@call.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 
 #--------------------------------------Port------------------------------------
 
-@cs.route('/read/port/<bridge>', methods=['GET'])
+@call.route('/read/port/<bridge>', methods=['GET'])
 @auth.login_required
 def get_port(bridge):
 	#check if bridge exists on pc
-	if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+	if len(str(sub_sean.bridge_pc(bridge))) == 0:
 		abort(404)
 
-	return jsonify({'Port': subprocess_sean.get_ports(bridge).splitlines()})	
+	return jsonify({'Port': sub_sean.get_ports(bridge).splitlines()})	
 
-@cs.route('/add/port/<bridge>', methods=['POST'])
+@call.route('/add/port/<bridge>', methods=['POST'])
 @auth.login_required
 def add_port(bridge):
       
 	#check if bridge already exists
-	if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+	if len(str(sub_sean.bridge_pc(bridge))) == 0:
 		abort(404)
 
 	if not request.json:
@@ -55,19 +55,19 @@ def add_port(bridge):
 	port = request.json['port']
 
         #check if interface already exists on pc
-        if len(str(subprocess_sean.int_pc(port))) == 0:
+        if len(str(sub_sean.int_pc(port))) == 0:
                 abort(404)
 
-	subprocess_sean.add_ports(bridge, port)
+	sub_sean.add_ports(bridge, port)
 	return jsonify({'Bridge': bridge,
 			'Port': port}), 201
 
-@cs.route('/delete/port/<bridge>', methods=['DELETE'])
+@call.route('/delete/port/<bridge>', methods=['DELETE'])
 @auth.login_required
 def del_port(bridge):
 	
 	#check if bridge exists
-	if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+	if len(str(sub_sean.bridge_pc(bridge))) == 0:
 		abort(404)
 
 	if not request.json or not 'port' in request.json:
@@ -76,83 +76,83 @@ def del_port(bridge):
 	port = request.json['port']
 
 	#check if port exists on bridge
-	if len(str(subprocess_sean.port_bridge(bridge, port))) == 0:
+	if len(str(sub_sean.port_bridge(bridge, port))) == 0:
 		abort(404)		
 
-	subprocess_sean.del_ports(bridge, port)
+	sub_sean.del_ports(bridge, port)
 	
 	#check if delete was successful
-	if len(str(subprocess_sean.port_bridge(bridge, port))) != 0:
+	if len(str(sub_sean.port_bridge(bridge, port))) != 0:
 		abort(404)	
 
 	return jsonify({'Result': True})
 
-@cs.route('/update/port/<bridge>/<port>', methods=['PUT'])
+@call.route('/update/port/<bridge>/<port>', methods=['PUT'])
 @auth.login_required
 def update_port(bridge, port):
 
 	#check if bridge exist
-	if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+	if len(str(sub_sean.bridge_pc(bridge))) == 0:
 		abort(404)
 	
 	#check if port exist on bridge
-	if len(str(subprocess_sean.port_bridge(bridge, port))) == 0:
+	if len(str(sub_sean.port_bridge(bridge, port))) == 0:
 		abort(404)
 
 	if not request.json or not 'action' in request.json:
 		abort(400)
 
 	action = request.json['action']
-	subprocess_sean.update_ports(bridge, port, action)
+	sub_sean.update_ports(bridge, port, action)
 	return jsonify({'Bridge': bridge,
 			'Port': port,
 			'Action': action})
 
 #-------------------------------Bridge-----------------------------------------
 
-@cs.route('/read/bridge/<bridge>', methods=['GET'])
+@call.route('/read/bridge/<bridge>', methods=['GET'])
 @auth.login_required
 def get_bridge(bridge):
         #check if bridge exists on pc
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
-        return jsonify({'Bridge': subprocess_sean.get_bridge(bridge).splitlines()})
+        return jsonify({'Bridge': sub_sean.get_bridge(bridge).splitlines()})
 
-@cs.route('/add/bridge/<bridge>', methods=['POST'])
+@call.route('/add/bridge/<bridge>', methods=['POST'])
 @auth.login_required
 def add_bridge(bridge):
 
         #check if bridge already exists
-        if len(str(subprocess_sean.bridge_pc(bridge))) != 0:
+        if len(str(sub_sean.bridge_pc(bridge))) != 0:
                 abort(404)
 
-        subprocess_sean.add_bridge(bridge)
+        sub_sean.add_bridge(bridge)
         return jsonify({'Bridge': bridge}), 201
 
-@cs.route('/delete/bridge/<bridge>', methods=['DELETE'])
+@call.route('/delete/bridge/<bridge>', methods=['DELETE'])
 @auth.login_required
 def del_bridge(bridge):
 
         #check if bridge exists
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
-        subprocess_sean.del_bridge(bridge)
+        sub_sean.del_bridge(bridge)
         return jsonify({'Result': True})
 
-@cs.route('/update/bridge/<bridge>', methods=['PUT'])
+@call.route('/update/bridge/<bridge>', methods=['PUT'])
 @auth.login_required
 def update_bridge(bridge):
         #check if bridge exists on pc
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
         if not request.json or not 'options' in request.json:
                 abort(400)
 
         options = request.json['options']
-        subprocess_sean.update_bridge(bridge,options)
+        sub_sean.update_bridge(bridge,options)
 
 	return jsonify({'Bridge': bridge,
 			'Options': options})
@@ -160,22 +160,22 @@ def update_bridge(bridge):
 
 #----------------------------------Port mirror---------------------------------
 
-@cs.route('/read/mirror/<mirror>', methods=['GET'])
+@call.route('/read/mirror/<mirror>', methods=['GET'])
 @auth.login_required
 def get_mirror(mirror):
 	
 	#check if mirror exists
-	if len(str(subprocess_sean.check_mirror(mirror))) == 0:
+	if len(str(sub_sean.check_mirror(mirror))) == 0:
 		abort(404)
 
-        return jsonify({'Mirror': subprocess_sean.get_mirror(mirror).splitlines()})
+        return jsonify({'Mirror': sub_sean.get_mirror(mirror).splitlines()})
 	
-@cs.route('/add/mirror/<bridge>', methods=['POST'])
+@call.route('/add/mirror/<bridge>', methods=['POST'])
 @auth.login_required
 def add_mirror(bridge):
 
         #check if bridge already exists
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(400)
 	
         if not request.json or not 'port1' in request.json:
@@ -212,30 +212,30 @@ def add_mirror(bridge):
         output = request.json['output']
 
         #check if port already exists
-        if len(str(subprocess_sean.int_pc(port2))) == 0:
+        if len(str(sub_sean.int_pc(port2))) == 0:
                 abort(404)
 
-        if len(str(subprocess_sean.int_pc(port4))) == 0:
+        if len(str(sub_sean.int_pc(port4))) == 0:
                 abort(404)
 
         #check if port exist on bridge
-        if len(str(subprocess_sean.port_bridge(bridge, port2))) == 0:
+        if len(str(sub_sean.port_bridge(bridge, port2))) == 0:
                 abort(404)
 
-        if len(str(subprocess_sean.port_bridge(bridge, port4))) == 0:
+        if len(str(sub_sean.port_bridge(bridge, port4))) == 0:
                 abort(404)
 	
         if port2 == port4:
                 abort(400)
 
         #check if mirror with same name already exists
-        if len(str(subprocess_sean.check_mirror(name))) != 0:
+        if len(str(sub_sean.check_mirror(name))) != 0:
                 abort(400)
 
-        subprocess_sean.add_mirror(bridge, port1, port2, port3, port4, name, dest, source, output)
+        sub_sean.add_mirror(bridge, port1, port2, port3, port4, name, dest, source, output)
 
         #check if mirror was added successfully
-        if len(str(subprocess_sean.check_mirror(mirror))) == 0:
+        if len(str(sub_sean.check_mirror(name))) == 0:
                 abort(400)
 
         return jsonify({'Bridge': bridge,
@@ -245,12 +245,12 @@ def add_mirror(bridge):
 			'Output': output}), 201
 
 
-@cs.route('/update/mirror/<bridge>', methods=['PUT'])
+@call.route('/update/mirror/<bridge>', methods=['PUT'])
 @auth.login_required
 def update_mirror(bridge):
 
         #check if bridge already exists
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
         if not request.json or not 'port1' in request.json:
@@ -287,23 +287,23 @@ def update_mirror(bridge):
         output = request.json['output']
 
         #check if port already exists
-        if len(str(subprocess_sean.int_pc(port2))) == 0:
+        if len(str(sub_sean.int_pc(port2))) == 0:
                 abort(404)
 
-        if len(str(subprocess_sean.int_pc(port4))) == 0:
+        if len(str(sub_sean.int_pc(port4))) == 0:
                 abort(404)
 
         #check if port exist on bridge
-        if len(str(subprocess_sean.port_bridge(bridge, port2))) == 0:
+        if len(str(sub_sean.port_bridge(bridge, port2))) == 0:
                 abort(404)
 
-        if len(str(subprocess_sean.port_bridge(bridge, port4))) == 0:
+        if len(str(sub_sean.port_bridge(bridge, port4))) == 0:
                 abort(404)
 
         if port2 == port4:
                 abort(400)
 
-        subprocess_sean.add_mirror(bridge, port1, port2, port3, port4, name, dest, source, output)
+        sub_sean.add_mirror(bridge, port1, port2, port3, port4, name, dest, source, output)
 
         return jsonify({'Bridge': bridge,
                         'Name': name,
@@ -312,44 +312,44 @@ def update_mirror(bridge):
                         'Output': output})
 
 
-@cs.route('/delete/mirror/<bridge>/<mirror>', methods=['DELETE'])
+@call.route('/delete/mirror/<bridge>/<mirror>', methods=['DELETE'])
 @auth.login_required
 def del_mirror(bridge, mirror):
 
         #check if bridge exists
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
         #check if mirror exists
-        if len(str(subprocess_sean.check_mirror(mirror))) == 0:
+        if len(str(sub_sean.check_mirror(mirror))) == 0:
                 abort(404)
 
-        subprocess_sean.del_mirror(bridge, mirror)
+        sub_sean.del_mirror(bridge, mirror)
 
         #check if mirror gets deleted successfully
-        if len(str(subprocess_sean.check_mirror(mirror))) != 0:
+        if len(str(sub_sean.check_mirror(mirror))) != 0:
                 abort(400)
 
         return jsonify({'Result': True})
 
 #-------------------------NetFlow----------------------------------------------
 
-@cs.route('/read/netflow/<bridge>', methods=['GET'])
+@call.route('/read/netflow/<bridge>', methods=['GET'])
 @auth.login_required
 def get_netflow(bridge):
         #check if bridge exists on pc
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
         return jsonify({'Bridge': bridge,
-                        'Netflow': subprocess_sean.get_netflow(bridge).splitlines()})
+                        'Netflow': sub_sean.get_netflow(bridge).splitlines()})
 
 
-@cs.route('/update/netflow/<bridge>', methods=['PUT'])
+@call.route('/update/netflow/<bridge>', methods=['PUT'])
 @auth.login_required
 def update_netflow(bridge):
         #check if bridge exists on pc
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
 	if not request.json or not 'options' in request.json:
@@ -357,17 +357,17 @@ def update_netflow(bridge):
 
         options = request.json['options']
 
-        subprocess_sean.update_netflow(bridge,options)
+        sub_sean.update_netflow(bridge,options)
 
         return jsonify({'Bridge': bridge,
                         'Options': options})
 
 
-@cs.route('/add/netflow/<bridge>', methods=['POST'])
+@call.route('/add/netflow/<bridge>', methods=['POST'])
 @auth.login_required
 def add_netflow(bridge):
         #check if bridge exists on pc
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
         if not request.json or not 'target' in request.json:
@@ -378,24 +378,24 @@ def add_netflow(bridge):
 	target = request.json['target']
         timeout = request.json['timeout']
 
-        subprocess_sean.add_netflow(bridge, target, timeout)
+        sub_sean.add_netflow(bridge, target, timeout)
 
         return jsonify({'Bridge': bridge,
                         'Target': target,
 			'Timeout': timeout})
 
 
-@cs.route('/delete/netflow/<bridge>', methods=['DELETE'])
+@call.route('/delete/netflow/<bridge>', methods=['DELETE'])
 @auth.login_required
 def del_netflow(bridge):
         #check if bridge exists on pc
-        if len(str(subprocess_sean.bridge_pc(bridge))) == 0:
+        if len(str(sub_sean.bridge_pc(bridge))) == 0:
                 abort(404)
 
-        subprocess_sean.del_netflow(bridge)
+        sub_sean.del_netflow(bridge)
 
         return jsonify({'Result': True})
 
 
 if __name__ == '__main__':
-	cs.run(debug=True)
+	call.run(debug=True)
